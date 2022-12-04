ea0f6d8040a0708e7b95bfe57ec32e3357f58a99
Mark Evans <mark@riotenterprises.com>
2022-12-04 14:48:31 +0000

    feat: :sparkles: Initial Test Project

---
+++ src/WorkerConsole.Sample/WorkerService.Tests/WorkerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using WorkerService;
+using Xunit;
+
+namespace WorkerService.Tests
+{
+    public class WorkerTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<ILogger<Worker>> mockLogger;
+
+        public WorkerTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockLogger = this.mockRepository.Create<ILogger<Worker>>();
+        }
+
+        private Worker CreateWorker()
+        {
+            return new Worker(
+                this.mockLogger.Object);
+        }
+
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var worker = this.CreateWorker();
+
+            // Act
+
+
+            // Assert
+            Assert.NotNull(worker);
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
